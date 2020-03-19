4276f30e305e49107222bd7afd09367921824331
Dawid Przybylski <dawid.przybylski@symphony-solutions.eu>
2020-03-19 12:06:12 +0100

    Initial Commit

---
+++ AliorKantorCurrency/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AliorKantorCurrency
+//
+//  Created by Dawid Przybylski on 19/03/2020.
+//  Copyright © 2020 Dawid Przybylski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AliorKantorCurrencyTests/AliorKantorCurrencyTests.swift
@@ -0,0 +1,34 @@
+//
+//  AliorKantorCurrencyTests.swift
+//  AliorKantorCurrencyTests
+//
+//  Created by Dawid Przybylski on 19/03/2020.
+//  Copyright © 2020 Dawid Przybylski. All rights reserved.
+//
+
+import XCTest
+@testable import AliorKantorCurrency
+
+class AliorKantorCurrencyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
